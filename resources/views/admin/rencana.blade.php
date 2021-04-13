f1de172d4b968d0a20cd4c8ab6f1d7d764281bac
latifatuisnaini <50396881+latifatuisnaini@users.noreply.github.com>
2021-04-13 10:50:26 +0700

    update grafik rencana

--- resources/views/admin/rencana.blade.php
+++ resources/views/admin/rencana.blade.php
@@ -121,6 +121,46 @@ table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataT
 <div class="intro-y box mt-5">
     <!--Container-->
     <!--Card-->
+
+    <!-- Grafik Rencana -->
+    <div class="col-span-12 lg:col-span-8 mt-8">
+            <div class="intro-y block sm:flex items-center h-10">
+                <h2 class="text-lg font-medium truncate mr-5">
+                    Grafik Jumlah Proyek Tahun {{ $current_year }}
+                </h2>
+            </div>
+            <div class="intro-y box p-5 mt-12 sm:mt-5">
+                <div class="flex flex-col xl:flex-row xl:items-center">
+                    <div class="flex mb-5">
+                        <div>
+                            <div class="text-theme-20 dark:text-gray-300 text-lg xl:text-xl font-bold">{{ $jml_proyek_this_month }}</div>
+                            <div class="text-gray-600 dark:text-gray-600">Bulan Ini</div>
+                        </div>
+                        <div class="w-px h-12 border border-r border-dashed border-gray-300 dark:border-dark-5 mx-4 xl:mx-6"></div>
+                        <div>
+                            <div class="text-gray-600 dark:text-gray-600 text-lg xl:text-xl font-medium">{{ $jml_proyek_last_month }}</div>
+                            <div class="text-gray-600 dark:text-gray-600">Bulan Lalu</div>
+                        </div>
+                    </div>
+                    <!--
+                    <div class="dropdown xl:ml-auto mt-5 xl:mt-0">
+                        <button class="dropdown-toggle button font-normal border dark:border-dark-5 text-white dark:text-gray-300 relative flex items-center text-gray-700"> Filter by Category <i data-feather="chevron-down" class="w-4 h-4 ml-2"></i> </button>
+                        <div class="dropdown-box w-40">
+                            <div class="dropdown-box__content box dark:bg-dark-1 p-2 overflow-y-auto h-32"> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">PC & Laptop</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Smartphone</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Electronic</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Photography</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Sport</a> </div>
+                        </div>
+                    </div>
+                    -->
+                </div>
+                <div class="report-chart">
+                <!--
+                    <canvas id="report-line-chart" height="160" class="mt-6"></canvas>
+                    -->
+                    <canvas id="line-chart" height="160" class="mt-6"></canvas>
+                </div>
+            </div>
+        </div>
+
+    <!-- End Grafik Rencana -->
     
     <a href ="javascript:;" data-toggle="modal" data-target="#tambah_rencana" class="button mb-5 mr-6 mt-4 flex items-center justify-center bg-theme-1 text-white tombol-tambah-rencana" style="float:right;" ><i data-feather="plus-circle" class="w-6 h-6 mr-2"></i>Tambah Rencana Proyek</a>
     
@@ -261,6 +301,7 @@ table.dataTable.dtr-inline.collapsed>tbody>tr>td:first-child:before, table.dataT
 <!--Datatables -->
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script>
 $(document).ready(function() {
 
@@ -271,6 +312,31 @@ $(document).ready(function() {
         .columns.adjust()
         .responsive.recalc();
 
+var ctx = document.getElementById('line-chart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line', 
+  data: {
+    labels: ['PV','EV','AC','RENCANA','REALISASI'],
+    datasets: [{ 
+        data: [
+            <?php echo $pv ?>,
+            <?php echo $ev ?>,
+            <?php echo $ac ?>,
+            <?php echo $rencana ?>,
+            <?php echo $realisasi ?>,
+        ],
+        borderColor: "#3e95cd",
+        fill: false
+      }]
+  },
+  options: {
+    legend: {
+        display: false
+    }
+  }
+});
+
+</script>
 
     
 
