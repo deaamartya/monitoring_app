f420e5334b8904532cd153cb08c6bfab05fb94d3
latifatuisnaini <50396881+latifatuisnaini@users.noreply.github.com>
2021-04-13 09:11:14 +0700

    update dashboard admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Progress;
 use Illuminate\Support\Carbon;
 use DB;
 
 class AdminController extends Controller
 {
-    
+    //
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Progress;
+use Illuminate\Support\Carbon;
 
 class HomeController extends Controller
 {
@@ -21,8 +23,22 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('dashboard');
+    public function index(){
+        $progress=Progress::all();
+        $current_year = date('Y');
+        $jml_proyek_this_month  = Progress::whereMonth(
+            'tanggal', '=', Carbon::now()->month
+        )->whereYear('TANGGAL', date('Y'))->count();
+        $jml_proyek_last_month  = Progress::whereMonth(
+            'tanggal', '=', Carbon::now()->subMonth()->month
+        )->whereYear('TANGGAL', date('Y'))->count();
+
+        $pv = Progress::all();
+        $ev = Progress::all();
+        $ac = Progress::all();
+        $rencana= Progress::all();
+        $realisasi = Progress::all();
+
+        return view('dashboard', compact('progress', 'current_year', 'jml_proyek_this_month', 'jml_proyek_last_month', 'pv', 'ev', 'ac', 'rencana', 'realisasi'));
     }
 }
