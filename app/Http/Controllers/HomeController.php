b4527a33d08d70cf750e3ca8e501cc9777e1910a
latifatuisnaini <50396881+latifatuisnaini@users.noreply.github.com>
2021-04-14 08:16:55 +0700

    update grafik

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Progress;
+use App\Models\Proyek;
+use App\Models\Tipe;
 use Illuminate\Support\Carbon;
 
 class HomeController extends Controller
@@ -26,19 +28,26 @@ class HomeController extends Controller
     public function index(){
         $progress=Progress::all();
         $current_year = date('Y');
-        $jml_proyek_this_month  = Progress::whereMonth(
-            'tanggal', '=', Carbon::now()->month
-        )->whereYear('TANGGAL', date('Y'))->count();
-        $jml_proyek_last_month  = Progress::whereMonth(
-            'tanggal', '=', Carbon::now()->subMonth()->month
-        )->whereYear('TANGGAL', date('Y'))->count();
-
-        $pv = Progress::all();
-        $ev = Progress::all();
-        $ac = Progress::all();
-        $rencana= Progress::all();
-        $realisasi = Progress::all();
-
-        return view('dashboard', compact('progress', 'current_year', 'jml_proyek_this_month', 'jml_proyek_last_month', 'pv', 'ev', 'ac', 'rencana', 'realisasi'));
+        $jml_proyek_this_month  = Proyek::all()->count();
+        $jml_proyek_last_month  = Proyek::whereMonth(
+            'START_PROYEK', '=', Carbon::now()->subMonth()->month
+        )->whereYear('START_PROYEK', date('Y'))->count();
+        $tipe=Tipe::all();
+        $pv = Tipe::select('ID_TIPE')
+        ->where('ID_TIPE', '=', 1)
+        ->get();
+        $ev = Tipe::select('ID_TIPE')
+        ->where('ID_TIPE', '=', 2)
+        ->get();
+        $ac = Tipe::select('ID_TIPE')
+        ->where('ID_TIPE', '=', 3)
+        ->get();
+        $rencana= Tipe::select('ID_TIPE')
+        ->where('ID_TIPE', '=', 4)
+        ->get();
+        $realisasi = Tipe::select('ID_TIPE')
+        ->where('ID_TIPE', '=', 5)
+        ->get();
+        return view('dashboard', compact('progress', 'current_year', 'jml_proyek_this_month', 'jml_proyek_last_month', 'tipe', 'pv', 'ev', 'ac', 'rencana', 'realisasi'));
     }
 }
